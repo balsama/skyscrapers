607f16c89a1f9229d5ce091b8a2675c51ac3a279
Adam Balsam <balsam.adam@gmail.com>
2016-09-28 15:45:35 -0400

    Fixes #5 #4: Make constraints chainable and desired visible optional

--- src/View.php
+++ src/View.php
@@ -1,18 +1,197 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: adam.balsam
+ * Date: 9/28/16
+ * Time: 12:12 PM
+ */
 
 namespace Balsama\Skyscrapers;
-use SebastianBergmann\Exporter\Exception;
 use NajiDev\Permutation;
 
-/**
- * Class View
- * @package Balsama\Skyscrapers
- *
- * Provides methods to help determine valid order of Skyscrapers given the
- * number of Skyscraper Buildings (blocks) and the desired number of those which
- * are visible.
- */
+
 class View {
+
+  /**
+   * @var array solutions
+   *   The possible order of skyscrapers given defined constraints, number of
+   *   blocks and desired visible skyscrapers.
+   */
+  private $solutions;
+
+  /**
+   * @param array $solutions
+   */
+  private function setSolutions($solutions) {
+    $this->solutions = $solutions;
+  }
+
+  /**
+   * @return array
+   */
+  public function getSolutions() {
+    return $this->solutions;
+  }
+
+  /**
+   * View constructor.
+   */
+  public function __construct() {
+    $permutations = $this->getAllPermutations();
+    $this->setSolutions($permutations);
+  }
+
+  /**
+   * @var null or int
+   *   The number that should be visible from a view, if known.
+   */
+  private $desiredVisible = null;
+  public function setDesiredVisible($desiredVisible) {
+    $this->desiredVisible = $desiredVisible;
+    $this->applyVisibleFilter();
+  }
+
+  /**
+   * @return null or int
+   */
+  private function getDesiredVisible() {
+    return $this->desiredVisible;
+  }
+
+  /**
+   * Filters $solutions to only include results in which the $desiredVisible
+   * number of skyscrapers are visible.
+   */
+  private function applyVisibleFilter() {
+    $desiredVisible = $this->getDesiredVisible();
+    $currentSolutions = $this->getSolutions();
+
+    $validPermutations = [];
+    foreach ($currentSolutions as $currentSolution) {
+      if ($this->getVisible($currentSolution) == $desiredVisible) {
+        $validPermutations[] = $currentSolution;
+      }
+    }
+    $this->setSolutions($validPermutations);
+  }
+
+  /**
+   * Applies all currently defined $constraints.
+   */
+  private function applyConstraints() {
+    $currentSolutions = $this->getSolutions();
+    $constraints = $this->getConstraints();
+    if (empty($constraints)) {
+      return;
+    }
+    foreach ($constraints as $constraint) {
+      $this->applyConstraint($currentSolutions, $constraint);
+    }
+  }
+
+  /**
+   * Removes permutations from $solutions that don't meet the passed
+   * $constraint.
+   *
+   * @param $currentSolutions
+   *   The subset of all permutations that are currently valid.
+   * @param $constraint
+   *   A single constraint to be applied.
+   */
+  private function applyConstraint($currentSolutions, $constraint) {
+    $validConstrainedPermutations = [];
+    foreach ($currentSolutions as $currentSolution) {
+      switch($constraint['operator']) {
+        case ">":
+          if ($currentSolution[$constraint['position']] > $constraint['value']) {
+            $validConstrainedPermutations[] = $currentSolution;
+          }
+          break;
+        case "<":
+          if ($currentSolution[$constraint['position']] < $constraint['value']) {
+            $validConstrainedPermutations[] = $currentSolution;
+          }
+          break;
+        case "==":
+          if ($currentSolution[$constraint['position']] == $constraint['value']) {
+            $validConstrainedPermutations[] = $currentSolution;
+          }
+          break;
+        case "!=":
+          if ($currentSolution[$constraint['position']] != $constraint['value']) {
+            $validConstrainedPermutations[] = $currentSolution;
+          }
+      }
+    }
+    $this->setSolutions($validConstrainedPermutations);
+  }
+
+  /**
+   * @var array
+   *   Stores any constraints that have been set on possible building heights.
+   */
+  private $constraints = [];
+
+  /**
+   * Sets a constraint.
+   *
+   * @param int $position
+   *   The position of the building (1 is the closest to you)
+   * @param string $operator
+   *   The operator for the function. Allowed values:
+   *   - < (less then)
+   *   - > (greater then)
+   *   - == (equals)
+   * @param int $value
+   *   The value for the operator to act on.
+   * @throws \Exception
+   */
+  public function addConstraint($position, $operator, $value) {
+    if ($position > $this->getBlocks()) {
+      throw new \Exception('Position must be less than total blocks.');
+    }
+    $position--;
+    if (!in_array($operator, ['>', '<', '==', '!='])) {
+      throw new \Exception('Operator must be one of \'>\', \'<\', \'==\', \'!=\'');
+    }
+    if ((!is_int($value)) || ($value < 1)) {
+      throw new \Exception('Value must be a positive integer');
+    }
+    $this->constraints = $this->getConstraints();
+    $this->constraints[] = [
+      'position' => $position,
+      'operator' => $operator,
+      'value' => $value,
+    ];
+    $this->applyConstraints();
+  }
+
+  /**
+   * Gets current defined constraints.
+   */
+  private function getConstraints() {
+    return $this->constraints;
+  }
+
+  /**
+   * Gets all the possible permutations of skyscrapers given the block length.
+   */
+  private function getAllPermutations() {
+    $blocks = $this->getBlocks();
+
+    // Convert the number of blocks into the height of buildings within it
+    // (given that only one building of each height can exist in a row or
+    // column).
+    $buildings = $this->integerToArray($blocks);
+    $permutations = [];
+
+    $iterations = new Permutation\PermutationIterator($buildings);
+    foreach ($iterations as $iteration) {
+      $permutations[] = $iteration;
+    }
+
+    return $permutations;
+  }
 
   /**
    * The number of blocks to the furthest skyscraper from the view.
@@ -33,6 +212,8 @@ class View {
       throw new \Exception("We would need more power for that.");
     }
     $this->blocks = $blocks;
+    $permutations = $this->getAllPermutations();
+    $this->setSolutions($permutations);
   }
 
   /**
@@ -43,77 +224,6 @@ class View {
    */
   public function getBlocks() {
     return $this->blocks;
-  }
-
-  /**
-   * Gets all the possible permutations of skyscrapers given the block length.
-   */
-  public function getPermutations() {
-    $blocks = $this->getBlocks();
-
-    // Convert the number of blocks into the height of buildings within it
-    // (given that only one building of each height can exist in a row or
-    // column).
-    $buildings = $this->integerToArray($blocks);
-    $permutations = [];
-
-    $iterations = new Permutation\PermutationIterator($buildings);
-    foreach ($iterations as $iteration) {
-      $permutations[] = $iteration;
-    }
-
-    return $permutations;
-  }
-
-  /**
-   * The desired number of buildings visible from a view.
-   *
-   * @var int
-   */
-  private $desiredVisible = 3;
-
-  /**
-   * Sets the desired number of visible buildings.
-   *
-   * @param int $desiredVisible
-   *    The number of desired visible buildings
-   * @throws \Exception
-   */
-  public function setDesiredVisible($desiredVisible) {
-    if ($desiredVisible > $this->getBlocks()) {
-      throw new \Exception("Desired visible cannot be larger than total number of blocks");
-    }
-    $this->desiredVisible = $desiredVisible;
-  }
-
-  /**
-   * Gets the number of desired visible buildings
-   *
-   * @return int
-   */
-  public function getDesiredVisible() {
-    return $this->desiredVisible;
-  }
-
-  /**
-   * Returns all valid permutations.
-   *
-   * Given the number of Skyscrapers (block length) and the desired number
-   * visible, returns all possible orders of those skyscrapers.
-   *
-   * @return array
-   *   The valid permutations
-   */
-  public function getValidPermutations() {
-    $desiredVisible = $this->getDesiredVisible();
-    $allPermutations = $this->getPermutations();
-    $validPermutations = [];
-    foreach ($allPermutations as $possibleValidPermutation) {
-      if ($this->getVisible($possibleValidPermutation) == $desiredVisible) {
-        $validPermutations[] = $possibleValidPermutation;
-      }
-    }
-    return $validPermutations;
   }
 
   /**
@@ -132,82 +242,6 @@ class View {
   }
 
   /**
-   * @var null
-   *   Stores any constraints that have been set on possible building heights.
-   */
-  private $constraint = [];
-
-  /**
-   * Sets a constraint.
-   *
-   * @param int $position
-   *   The position of the building (1 is the closest to you)
-   * @param string $operator
-   *   The operator for the function. Allowed values:
-   *   - < (less then)
-   *   - > (greater then)
-   *   - == (equals)
-   * @param int $value
-   *   The value for the operator to act on.
-   * @throws \Exception
-   */
-  public function setConstraint($position, $operator, $value) {
-    if ($position > $this->getBlocks()) {
-      throw new \Exception('Position must be less than total blocks.');
-    }
-    $position--;
-    if (!in_array($operator, ['>', '<', '=='])) {
-      throw new Exception('Operator must be one of \'>\', \'<\', \'==\'');
-    }
-    if ((!is_int($value)) || ($value < 1)) {
-      throw new Exception('Value must be a positive integer');
-    }
-    $this->constraint = [
-      'position' => $position,
-      'operator' => $operator,
-      'value' => $value,
-    ];
-  }
-
-  /**
-   * Gets constraint if defined.
-   *
-   * @return
-   *   The current constraint
-   */
-  public function getConstraint() {
-    return $this->constraint;
-  }
-
-  public function getValidConstrainedPermutations() {
-    $validPermutations = $this->getValidPermutations();
-    $constraint = $this->getConstraint();
-    if (!$constraint) {
-      return $validPermutations;
-    }
-    $validConstrainedPermutations = [];
-    foreach ($validPermutations as $validPermutation) {
-      switch($constraint['operator']) {
-        case ">":
-          if ($validPermutation[$constraint['position']] > $constraint['value']) {
-            $validConstrainedPermutations[] = $validPermutation;
-          }
-          break;
-        case "<":
-          if ($validPermutation[$constraint['position']] < $constraint['value']) {
-            $validConstrainedPermutations[] = $validPermutation;
-          }
-          break;
-        case "==":
-          if ($validPermutation[$constraint['position']] == $constraint['value']) {
-            $validConstrainedPermutations[] = $validPermutation;
-          }
-      }
-    }
-    return $validConstrainedPermutations;
-  }
-
-  /**
    * Converts an integer into an array of integers that come before it.
    *
    * @param $int
@@ -223,5 +257,4 @@ class View {
     }
     return $depth;
   }
-
 }
--- tests/ViewTest.php
+++ tests/ViewTest.php
@@ -4,59 +4,100 @@ use Balsama\Skyscrapers\View;
 
 class ViewTest extends PHPUnit_Framework_TestCase {
 
-  public function testViewSetGetBlocks() {
+  /**
+   * Get results with no constraints or overrides
+   */
+  public function testDefaultResults() {
     $view = new View();
-    $set_blocks = 5;
-    $view->setBlocks($set_blocks);
-    $get_blocks = $view->getBlocks();
-    $this->assertTrue($set_blocks == $get_blocks);
+
+    // Asserts that the block length is three without setting it
+    $this->assertTrue(($view->getBlocks() == 3));
+
+    // Asserts that there are six solutions by default given the default block
+    // length of 3.
+    $results = $view->getSolutions();
+    $this->assertTrue(count($results) == 6);
+
+    // Asserts that each solution is an array with three skyscrapers.
+    foreach ($results as $result) {
+      $this->assertTrue((count($result) == 3));
+      foreach ($result as $skyscraper) {
+        $this->assertTrue(is_numeric($skyscraper));
+      }
+    }
   }
 
-  public function testGetPermutations() {
+  /**
+   * Add single and multiple constraints of each type and confirm that the
+   * results are correct.
+   */
+  public function testConstrainedResults() {
     $view = new View();
-    $this->assertTrue(count($view->getPermutations()) == 6);
+    $view->addConstraint(1, '==', 1);
 
-    $view->setBlocks(2);
-    $this->assertTrue(count($view->getPermutations()) == 2);
+    // Asserts that all solutions start with 1
+    $results = $view->getSolutions();
+    foreach ($results as $result) {
+      $this->assertTrue(($result[0] === 1));
+    }
+    // Asserts that only two results exist that start with 1
+    $this->assertTrue((count($results) == 2));
+
+    // Asserts that adding a directly opposing constraint returns zero results
+    $view->addConstraint(1, '!=', 1);
+    $results = $view->getSolutions();
+    $this->assertTrue((count($results) == 0));
+
+    // Asserts that gt constraint works
+    $view = new View();
+    $view->addConstraint(1, ">", 1);
+    $results = $view->getSolutions();
+    foreach ($results as $result) {
+      $this->assertTrue($result[0] > 1);
+    }
+
+    // Asserts that lt constraint can be chained
+    $view->addConstraint(3, "<", 3);
+    $results = $view->getSolutions();
+    foreach ($results as $result) {
+      $this->assertTrue(($result[0] > 1) && ($result[2] < 3));
+    }
+
   }
 
-  public function testGetVisible() {
+  /**
+   * Constrain results to those with a certain number of skyscrapers visible.
+   */
+  public function testDesiredVisibleResults() {
     $view = new View();
-    $shouldBeThree = $view->getVisible([1,2,3]);
-    $this->assertTrue($shouldBeThree === 3);
-
-    $shouldBeTwo = $view->getVisible([2,3,1]);
-    $this->assertTrue($shouldBeTwo === 2);
-
-    $shouldBeOne = $view->getVisible([3,1,2]);
-    $this->assertTrue($shouldBeOne === 1);
+    $view->setDesiredVisible(3);
+    $results = $view->getSolutions();
+    $this->assertTrue(count($results) == 1);
   }
 
-  public function testGetValidPermutations() {
+  /**
+   * Combine desired visible and place constraints
+   */
+  public function testConstraintsAndVisibleResults() {
     $view = new View();
-    $this->assertTrue(count($view->getValidPermutations()) === 1);
 
+    // Only two results should meet the criteria
+    $view->addConstraint(1, ">", 1);
     $view->setDesiredVisible(2);
-    $this->assertTrue(count($view->getValidPermutations()) === 3);
+    $results = $view->getSolutions();
+    $this->assertTrue(count($results) == 2);
+
+    // Adding a second constraint should limit results to one
+    $view->addConstraint(3, '==', 1);
+    $results = $view->getSolutions();
+    $this->assertTrue(count($results) == 1);
   }
 
-  public function testConstraints() {
+  public function testBlockLength() {
     $view = new View();
-
-    // Constrained permutation list is same as unconstrained list if no
-    // constraints are set.
-    $this->assertTrue($view->getValidPermutations() == $view->getValidConstrainedPermutations());
-
-    // No possible combinations show three if first building is 2
-    $view->setConstraint(1, "==", 2);
-    $this->assertTrue(empty($view->getValidConstrainedPermutations()));
-
-    // No possible combinations show three if first building greater than 1
-    $view->setConstraint(1, ">", 1);
-    $this->assertTrue(empty($view->getValidConstrainedPermutations()));
-
-    $view->setDesiredVisible(2);
-    $view->setConstraint(1, "<", 2);
-    $this->assertTrue(count($view->getValidConstrainedPermutations()) == 1);
+    $view->setBlocks(4);
+    $results = $view->getSolutions();
+    $this->assertTrue(count($results) == 24);
   }
+
 }
